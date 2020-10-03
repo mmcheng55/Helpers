4ff2547116371b518e246d78e9e5790edba774c8
mmcheng55 <56221863+mmcheng55@users.noreply.github.com>
2020-10-03 13:30:11 +0800

    TCPEcho

--- tcp_echo.py
+++ tcp_echo.py
@@ -3,9 +3,14 @@ import asyncio
 
 
 class TCPEcho:
-    def __init__(self, loop: asyncio.AbstractEventLoop = None):
+    def __init__(
+        self,
+        host: str = "localhost",
+        port: int = 5000,
+        loop: asyncio.AbstractEventLoop = None
+    ):
         self.loop = loop if loop is not None else asyncio.get_event_loop()
-        self.loop.create_task(asyncio.start_server(self.handle_client, 'localhost', 15555))
+        self.loop.create_task(asyncio.start_server(self.handle_client, host, port))
         self.Command = Commander()
 
     def run(self):
